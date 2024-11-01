c06b23653abeaa97fcfc235a0bc06541354e9474
Godwin Asuquo <godwinedetasuquo@gmail.com>
2024-11-01 11:02:10 -0700

    change content for pre footer

--- src/pages/landingPage/PreFooter.tsx
+++ src/pages/landingPage/PreFooter.tsx
@@ -4,9 +4,10 @@ const PreFooter = () => {
       data-aos="zoom-in-up"
       className="text-center mt-44 mb-32 mx-auto w-[90%] md:w-[70%] lg:w-[50%] max-w-7xl space-y-7"
     >
-      <h1 className="text-4xl lg:text-5xl font-semibold">Don't let financing hold back your solar business. </h1>
+      <h1 className="text-4xl lg:text-5xl font-semibold">Join the Synafare Network Today</h1>
       <p className="text-500 font-light text-lg text-center">
-        Partner with Synafare to retain customers and fuel your growth.
+        Ready to elevate your business with flexible financing for sustainable energy products? Partner with Synafare to
+        access competitive product financing and dedicated support. Contact us today to learn more!
       </p>
       <button className="px-3 py-2 text-sm border rounded-lg border-primary text-primary">Get Started for Free</button>
     </div>
