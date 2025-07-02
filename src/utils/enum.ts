538517151bb049999bf80976ee097eebbfae06b8
Divine Success <divinesuccess228@gmail.com>
2025-07-02 21:00:50 +0100

    create finance request page

--- src/utils/enum.ts
+++ src/utils/enum.ts
@@ -5,4 +5,5 @@ export enum PATHS {
   PACKAGE = '/solar-packages/:slug',
   INSTALLMENTAL_FORM = '/solar-packages/:package/installmental-form',
   PARTNER_REGISTRATION_FORM = '/partner-registration-form',
+  FINANCE_REQUEST_FORM = '/finance-request-form',
 }
